d59b33f93078e91515f0071460ab87627b27b08c
Zekai Zhang <zekaiz2@illinois.edu>
2021-11-15 22:00:33 -0600

    Update last mp last hopefully

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -11,6 +11,9 @@ SquareMaze::SquareMaze() {
 	
 }
 
+
+
+
 void SquareMaze::makeMaze(int width, int height) {
     width_ = width;
     height_ = height;
@@ -19,39 +22,45 @@ void SquareMaze::makeMaze(int width, int height) {
     disjoint_set.addelements(area);
     int dir=0;
     int second=0;
-    int rInt = random_int();
+
+    makeMazeH(dir,second,width);
     
+}
+
+void SquareMaze::makeMazeH(int dir,int second,int width){
+    
+
     while (disjoint_set.size(0) != area) {
         
-        makeMazeH(rInt,dir,second,width);
+    int rInt = random_int();
+    bool descende = random_bool();
+    if(descende) {
+            if(rInt + width >= area) continue;
+            if (disjoint_set.find(rInt) == disjoint_set.find(rInt + width)) continue;
+            dir = 1;
+            second = rInt + width;
+           
+        } else {
+            if((rInt + 1) % width == 0) continue;
+            if (disjoint_set.find(rInt) == disjoint_set.find(rInt + 1)) continue;
+            dir = 0;
+            second = rInt + 1;
+          
+        }
+        
+        
         setWall(rInt % width, rInt / width, dir, false);
         disjoint_set.setunion(rInt, second);
     }
 }
 
-bool SquareMaze::makeMazeH(int rInt,int dir,int second,int width){
-    
-    bool descende = random_bool();
-        
-        if(descende) {
-            if(rInt + width >= area) return true;
-            if (disjoint_set.find(rInt) == disjoint_set.find(rInt + width)) return true;
-            dir = 1;
-            second = rInt + width;
-           
-        } else {
-            if((rInt + 1) % width == 0) return true;
-            if (disjoint_set.find(rInt) == disjoint_set.find(rInt + 1)) return true;
-            dir = 0;
-            second = rInt + 1;
-          
-        }
-        return false;
-}
+
+
+
+
 
 bool SquareMaze::canTravel(int x, int y, int dir) const {
     if(height_ != 0 && width_ != 0) {
-        bool condition_1, condition_2, condition_3;
         if(dir == 0) {
             if(x < width_ - 1){
                 if(walls.at(y * width_ + x) != 1){
@@ -108,64 +117,69 @@ void SquareMaze::setWall(int x, int y, int dir, bool exists) {
 
 
 
-std::vector<int> SquareMaze::solveMaze() {
-    
-    std::vector<int> solved = solveMazeH();
-    std::vector<int> output;
-    int cur_longest_index = INT_MIN;
-    int cur_longest_dist = INT_MIN;
-    int m=(height_ - 1) * width_;
 
-    while(m<area){
-        if(solved.at(m) > cur_longest_dist) {
-            cur_longest_index = m;
-            cur_longest_dist = solved.at(m);
-        }
-        m++;
+vector<int> SquareMaze::solveMaze(){
+    int x, y, curr_idx;
+    vector<int> prev_cell;  
+    vector<int> distance;  
+    prev_cell.push_back(0); 
+    distance.push_back(0);
+    for(int i = 1; i < width_ * height_; i++){
+      prev_cell.push_back(-1); 
+      distance.push_back(0);
     }
-    
-    return findLongest(output,solved,cur_longest_dist);
-}
+    queue<int> bfs;
+    bfs.push(0);
+    while(!bfs.empty()){
+      curr_idx = bfs.front();
+      x = curr_idx % width_;
+      y = curr_idx / width_;
+      bfs.pop();
 
-std::vector<int> SquareMaze::solveMazeH() {
     
-    std::vector<int> path(3 * area, -1); 
-    
-    q.push(0); 
-    while(!q.empty()) {
-        auto v = q.front();
-        int x = v % width_;
-        int y = v / width_;
-        int temp=0;
-
-        for(int i=0;i<3;i++){
-            if(canTravel(x, y, i)) {
-                if(i==0) temp=1;
-                else if(i==1) temp=width_;
-                else if(i==2) temp=-1;
-                else if(i==3) temp= -1*width_;
-                if(path[v + temp] == -1) {
-                    path[v + temp] = path[v] + 1;
-                    path[v + temp + area] = i;
-                    path[v + temp + 2 * area] = v;
-                    q.push(v + temp);
+        for (int i=0;i<4;i++){
+            int temp=0;
+            if(i==0)temp=1;
+            if(i==1)temp=width_;
+            if(i==2)temp=-1;
+            if(i==3)temp=(-1*width_);
+            if(canTravel(x, y, i)){
+                if(prev_cell[curr_idx + temp] == -1){
+                bfs.push(curr_idx + temp);
+                prev_cell[curr_idx + temp] = curr_idx;
+                distance[curr_idx + temp] = distance[curr_idx] + 1;
                 }
             }
         }
-        q.pop();
     }
-    return path;
-}
-
-std::vector<int> SquareMaze::findLongest(std::vector<int> output,std::vector<int> solved,int cur_longest_index){
-    while(cur_longest_index) {
-        output.push_back(solved.at(cur_longest_index + area));
-        cur_longest_index = solved.at(cur_longest_index + 2 * area);
+    int longest_path = width_ * (height_ - 1);
+    for(int k = 0; k < width_; k++){
+      if(distance[width_ * (height_ - 1) + k] > distance[longest_path]){
+        longest_path = width_ * (height_ - 1) + k;
+      }
     }
-    std::reverse(output.begin(), output.end());
-    return output;
-}
+    destination = longest_path;
+    vector<int> solution;
+    curr_idx = destination; 
+    while(curr_idx != 0){
+      if(prev_cell[curr_idx] == curr_idx - 1){
+        solution.push_back(0);
+      }
+      if(prev_cell[curr_idx] == curr_idx - width_){
+        solution.push_back(1);
+      }
+      if(prev_cell[curr_idx] == curr_idx + 1){
+        solution.push_back(2);
+      }
+      if(prev_cell[curr_idx] == curr_idx + width_){
+        solution.push_back(3);
+      }
+      curr_idx = prev_cell[curr_idx];
+    }
+    reverse(solution.begin(), solution.end());
 
+  return solution;
+}
 
 PNG * SquareMaze::drawMaze()  {
     PNG * result = new PNG(width_ * 10 + 1, height_ * 10 + 1);
@@ -239,9 +253,12 @@ void SquareMaze::setPixel(HSLAPixel & pixel_){
     pixel_.a = 1;
 }
 
+
+
+
 bool SquareMaze::random_bool() {
-    
-    return rand()%2;
+    if(rand() % 2) return true;
+    return false;
 }
 
 int SquareMaze::random_int() {
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -10,13 +10,13 @@ class SquareMaze {
     public:
         SquareMaze();
         void makeMaze(int width, int height);
-        bool makeMazeH(int rInt,int dir,int second,int width);
+        void makeMazeH(int dir,int second,int width);
         bool canTravel(int x, int y, int dir) const;
         void setWall(int x, int y, int dir, bool exist);
         std::vector<int> solveMaze();
-        PNG* drawMaze() /*const*/;
+        PNG* drawMaze() ;
+        
         PNG* drawMazeWithSolution();
-        std::vector<int> findLongest(std::vector<int> output,std::vector<int> solved,int cur_longest_index);
         PNG * drawMazeH(PNG * result);
         void setPixel(HSLAPixel & pixel_);
 
@@ -24,10 +24,11 @@ class SquareMaze {
         int width_ = 0;
         int height_ = 0;
         int area = width_ * height_;
+        int destination=0;
         std::vector<int> walls;
         DisjointSets disjoint_set;
         bool random_bool();
         int random_int();
         std::queue<int> q;
-        std::vector<int> solveMazeH();
+        std::vector<int> bfs();
 };
